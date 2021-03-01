15051a30f238c4c284463ca70fdb0375de00211f
Nikitin Vladimir <vreservoirv@rambler.ru>
2021-03-01 16:15:23 +0300

    header refresh

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function () {
+  // Плавная прокрутка к якорю
   const anchors = document.querySelectorAll('.header__list a[href*="#"]')
   for (let anchor of anchors) {
     anchor.addEventListener('click', function (e) {
@@ -10,16 +11,23 @@ window.addEventListener('DOMContentLoaded', function () {
       })
     })
   }
-  // Header__menu select
-  const elements = document.querySelectorAll('.header-menu__select');
-  elements.forEach(el => {
-    const choices = new Choices(el, {
-      searchEnabled: false,
-      position: 'bottom',
-      shouldSort: false,
-    })
-  })
+  // Выпадающий список
+  const button = document.querySelectorAll('.header-menu__btn');
+  const drop = document.querySelectorAll('.header-menu__drop')
 
-  // var b = document.querySelector(".choices__list");
-  // b.setAttribute("disabled", "disabled");
+  button.forEach(el => {
+    el.addEventListener('click', (e) => {
+      drop.forEach(el => { el.classList.remove(('header-menu__drop--active')) })
+      e.currentTarget.closest('li').querySelector('.header-menu__drop').classList.toggle('header-menu__drop--active');
+    });
+  });
+
+  document.addEventListener('click', (e) => {
+    console.log(e.target)
+    if (!e.target.classList.contains('header-menu__drop') && !e.target.classList.contains('header-menu__btn')) {
+      drop.forEach(el => { el.classList.remove(('header-menu__drop--active')) })
+    }
+  });
+  // Скрол
+  // new SimpleBar(document.getElementById('myEl'));
 })
