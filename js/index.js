53e75e3ac5e127e0ec0aa497bf8395bfa653e108
Nikitin Vladimir <vreservoirv@rambler.ru>
2021-03-03 19:33:56 +0300

    Gallery slider

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function () {
   // Плавная прокрутка к якорю
-  const anchors = document.querySelectorAll('.header__list a[href*="#"]')
+  const anchors = document.querySelectorAll('.header__list a, .primary__text a[href*="#"]')
   for (let anchor of anchors) {
     anchor.addEventListener('click', function (e) {
       e.preventDefault()
@@ -21,13 +21,52 @@ window.addEventListener('DOMContentLoaded', function () {
       e.currentTarget.closest('li').querySelector('.header-menu__drop').classList.toggle('header-menu__drop--active');
     });
   });
-
   document.addEventListener('click', (e) => {
-    console.log(e.target)
     if (!e.target.classList.contains('header-menu__drop') && !e.target.classList.contains('header-menu__btn')) {
       drop.forEach(el => { el.classList.remove(('header-menu__drop--active')) })
     }
   });
   // Скрол
   // new SimpleBar(document.getElementById('myEl'));
+
+  // Слайдер
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+  });
+
+  const swiper1 = new Swiper('.swiper-container1', {
+    slidesPerView: 3, // сколько показывать
+    spaceBetween: 10, //расстояние между слайдами
+    pagination: { // Точки пагинации
+      el: '.swiper-pagination1', // класс контейнера для точек
+      // type: 'bullets', тип: точки
+      type: 'fraction', // тип: цифры
+      clickable: true, // возможность кликать по точкам
+    },
+
+    slideToClickedSlide: true, // переход к кликаемому слайду(слайд переходит в начало)
+
+    centeredSlides: true, // ставит кликнутый слайд в центр
+
+    loop: true, // зацикленность
+
+    navigation: { // стрелки навигации
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
+    },
+
+    // slidesPerGroup: 2,  сколько проматывать слайдов за один клик
+
+
+  });
+
+  //Селект
+  const element = document.querySelector('#gallery__select');
+  const choices = new Choices(element, {
+    searchEnabled: false,
+    noResultsText: 'Ничего не найдено',
+    position: 'bottom',
+    itemSelectText: '',
+  });
+
 })
