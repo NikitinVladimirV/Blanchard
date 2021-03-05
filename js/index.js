74d954cc6f5d429de8b208ac9219b22aea97bee5
Nikitin Vladimir <vreservoirv@rambler.ru>
2021-03-05 21:25:06 +0300

    Catalog accordion procces

--- js/index.js
+++ js/index.js
@@ -11,24 +11,29 @@ window.addEventListener('DOMContentLoaded', function () {
       })
     })
   }
-  // Выпадающий список
-  const button = document.querySelectorAll('.header-menu__btn');
-  const drop = document.querySelectorAll('.header-menu__drop')
+  // Выпадающий список(от преподавателя)
+  // const button = document.querySelectorAll('.header-menu__btn');
+  // const drop = document.querySelectorAll('.header-menu__drop')
 
-  button.forEach(el => {
-    el.addEventListener('click', (e) => {
-      drop.forEach(el => { el.classList.remove(('header-menu__drop--active')) })
-      e.currentTarget.closest('li').querySelector('.header-menu__drop').classList.toggle('header-menu__drop--active');
-    });
-  });
-  document.addEventListener('click', (e) => {
-    if (!e.target.classList.contains('header-menu__drop') && !e.target.classList.contains('header-menu__btn')) {
-      drop.forEach(el => { el.classList.remove(('header-menu__drop--active')) })
-    }
-  });
-  // Скрол
-  // new SimpleBar(document.getElementById('myEl'));
+  // button.forEach(el => {
+  //   el.addEventListener('click', (e) => {
+  //     drop.forEach(el => { el.classList.remove(('header-menu__drop--active')) })
+  //     e.currentTarget.closest('li').querySelector('.header-menu__drop').classList.toggle('header-menu__drop--active');
+  //   });
+  // });
+  // document.addEventListener('click', (e) => {
+  //   if (!e.target.classList.contains('header-menu__drop') && !e.target.classList.contains('header-menu__btn')) {
+  //     drop.forEach(el => { el.classList.remove(('header-menu__drop--active')) })
+  //   }
+  // });
 
+  // Выпадающий список(на основе пройденных уроков)
+  document.querySelectorAll('.header-menu__btn').forEach(function (drop) {
+    drop.addEventListener('click', function (add) {
+      add.target.classList.toggle('is-active')
+    })
+  });
+  
   // Слайдер
   const swiper = new Swiper('.swiper-container', {
     loop: true, // зацикленность
