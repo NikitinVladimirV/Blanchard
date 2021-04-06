bb97a5efad6bcbc9b3ddf2522d9347aa59d64a21
Nikitin Vladimir <vreservoirv@rambler.ru>
2021-04-06 14:27:39 +0300

    Modal windows created

--- js/index.js
+++ js/index.js
@@ -174,5 +174,34 @@ window.addEventListener('DOMContentLoaded', function () {
       event.target.classList.toggle('is-active')
     })
   })
+
+  const btns = document.querySelectorAll('.card');
+  const modalOverlay = document.querySelector('.modal-overlay');
+  const modals = document.querySelectorAll('.modal__artist');
+
+  btns.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      let path = e.currentTarget.getAttribute('data-path');
+      
+      modals.forEach((el) => {
+        el.classList.remove('modal__artist--visible');
+      });
+      
+      document.querySelector(`[data-target="${path}"]`).classList.add('modal__artist--visible');
+      modalOverlay.classList.add('modal-overlay--visible');
+    });
+  });
+
+  modalOverlay.addEventListener('click', (e) => {
+    console.log(e.target);
+    
+    if (e.target == modalOverlay) {
+      modalOverlay.classList.remove('modal-overlay--visible');
+      modals.forEach((el) => {
+        el.classList.remove('modal__artist--visible');
+      });
+    };
+  });
+  
   window.addEventListener('focusin', event => console.log(new Date, event.target));
 })
